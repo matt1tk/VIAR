7938975d0a12b487d35700247d7c2ccab923c25a
rasool <rsool_2001@yahoo.com>
2020-04-22 09:07:34 -0700

    Initial Commit
    - Background color now changes dynamically based upon if the driver is renamed or not
    - User is cautioned to restart the computer in order for changes to take full effect via a message box

--- VIAR/MainWindow.xaml.cs
+++ VIAR/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Windows;
 using System.IO;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace VIAR
 {
@@ -23,20 +24,26 @@ namespace VIAR
             if (File.Exists(@"C:\Program Files\Riot Vanguard\vgk.sys"))
             {
                 status.Text = "Status: Vanguard is enabled!";
+                this.Background = Brushes.DarkSeaGreen;
+                this.BorderBrush = Brushes.DarkOliveGreen;
             }
             else
             {
                 status.Text = "Status: Vanguard is disabled!";
+                this.Background = Brushes.IndianRed;
+                this.BorderBrush = Brushes.DarkRed;
             }
         }
         private void Button_Click(object sender, RoutedEventArgs e)//enable vanguard
         {
             File.Move(@"C:\Program Files\Riot Vanguard\vgk1.sys", @"C:\Program Files\Riot Vanguard\vgk.sys");
+            MessageBox.Show("A restart is required for these changes to take full effect!");
             UpdateStatus();
         }
         private void Button_Click_1(object sender, RoutedEventArgs e)//disable vanguard
         {
             File.Move(@"C:\Program Files\Riot Vanguard\vgk.sys", @"C:\Program Files\Riot Vanguard\vgk1.sys");
+            MessageBox.Show("A restart is required for these changes to take full effect!");
             UpdateStatus();
         }
 
