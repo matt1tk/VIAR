847c0e75dc613bdd023158287c0fa3517bf47ccf
Rasool221 <rsool_2001@yahoo.com>
2020-04-30 07:47:58 -0700

    Driver list update
    - Added new window to obtain driver list for more information

---
+++ VIAR/DriversList.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.IO;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Management;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace VIAR
+{
+    public partial class DriverWindow
+    {
+
+        List<string> driverList = new List<string>();
+
+        public DriverWindow()
+        {
+            InitializeComponent();
+            System.Windows.Forms.Application.EnableVisualStyles();
+            progressBar.Visibility = Visibility.Hidden;
+        }
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            this.DragMove();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            driversListBox.Items.Clear();
+
+            lblStatus.Content = "Status: Obtaining driver list...";
+
+            BackgroundWorker bw = new BackgroundWorker();
+            bw.DoWork += obtainList;
+            bw.RunWorkerCompleted += workFinished;
+            bw.RunWorkerAsync();
+            progressBar.Visibility = Visibility.Visible;
+        }
+
+        private void obtainList(object sender, DoWorkEventArgs e)
+        {
+            ManagementObjectSearcher helper = new ManagementObjectSearcher("SELECT * FROM Win32_PnPSignedDriver");
+
+            foreach (ManagementObject obj in helper.Get())
+            {
+                try
+                {
+                    if (helper.Get() == null)
+                        continue;
+
+                    if (obj.GetPropertyValue("DeviceName") == null)
+                        continue;
+                    
+                    this.driverList.Add(obj.GetPropertyValue("DeviceName").ToString());
+                }
+                catch (System.Reflection.TargetInvocationException)
+                {
+                    System.Windows.MessageBox.Show("An error has occured", "VIAR", MessageBoxButton.OK);
+                    break;
+                }
+            }
+        }
+
+        private void workFinished(object sender, RunWorkerCompletedEventArgs e)
+        {
+            foreach (string s in this.driverList)
+            {
+                driversListBox.Items.Add(s.ToString());
+            }
+
+            progressBar.Visibility = Visibility.Hidden;
+            lblStatus.Content = "Status: Idle";
+        }
+    }
+}
--- VIAR/MainWindow.xaml.cs
+++ VIAR/MainWindow.xaml.cs
@@ -4,11 +4,16 @@ using System.Windows;
 using System.IO;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Management;
+using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace VIAR
 {
     public partial class MainWindow
     {
+        List<string> drivers = new List<string>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -36,14 +41,30 @@ namespace VIAR
         }
         private void Button_Click(object sender, RoutedEventArgs e)//enable vanguard
         {
-            File.Move(@"C:\Program Files\Riot Vanguard\vgk1.sys", @"C:\Program Files\Riot Vanguard\vgk.sys");
-            MessageBox.Show("A restart is required for these changes to take full effect!");
+            try
+            {
+                File.Move(@"C:\Program Files\Riot Vanguard\vgk1.sys", @"C:\Program Files\Riot Vanguard\vgk.sys");
+                System.Windows.MessageBox.Show("A restart is required for these changes to take full effect!");
+
+            } catch (System.UnauthorizedAccessException)
+            {
+                System.Windows.MessageBox.Show("Error: Must run in administrator mode");
+            }
+
             UpdateStatus();
         }
         private void Button_Click_1(object sender, RoutedEventArgs e)//disable vanguard
         {
-            File.Move(@"C:\Program Files\Riot Vanguard\vgk.sys", @"C:\Program Files\Riot Vanguard\vgk1.sys");
-            MessageBox.Show("A restart is required for these changes to take full effect!");
+            try
+            {
+                File.Move(@"C:\Program Files\Riot Vanguard\vgk.sys", @"C:\Program Files\Riot Vanguard\vgk1.sys");
+                System.Windows.MessageBox.Show("A restart is required for these changes to take full effect!");
+            }
+            catch (System.UnauthorizedAccessException)
+            {
+                System.Windows.MessageBox.Show("Error: Must run in administrator mode");
+            }
+            
             UpdateStatus();
         }
 
@@ -54,7 +75,18 @@ namespace VIAR
 
         private void Button_Click_3(object sender, RoutedEventArgs e)//restart
         {
-            Process.Start("shutdown.exe", "-r -t 0");
+            MessageBoxResult result = System.Windows.MessageBox.Show("Are you sure you'd like to restart?", "VIAR", System.Windows.MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Process.Start("shutdown.exe", "-r -t 0");
+            }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            DriverWindow driverWindow = new DriverWindow();
+            driverWindow.Show();
         }
     }
 }
